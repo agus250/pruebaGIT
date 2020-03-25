36c536a052b1184095cee6da73351dc3efbab289
jpere247 <jpere247@M5266029.Go.JohnsonControls.com>
2020-03-24 21:28:22 -0500

    tdf

--- Testgoogle/src/google_page_object_model/Utilities.java
+++ Testgoogle/src/google_page_object_model/Utilities.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class Utilities {
 
 	public WebDriver driver;
